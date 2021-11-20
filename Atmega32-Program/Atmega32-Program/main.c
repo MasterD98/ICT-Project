384f8c5f3b9f8d729407edada1bc833fa3d69514
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-11-21 02:37:32 +0530

    keyboard added need to fix bug that display wrong character

--- Atmega32-Program/Atmega32-Program/main.c
+++ Atmega32-Program/Atmega32-Program/main.c
@@ -1,90 +1,173 @@
-/*  
-   LCD16x2 8 bit AVR ATmega16 interface
+/*
+   4x4 Keypad Interfacing with ATmega16/32
    http://www.electronicwings.com
-*/
+ */ 
+#include <avr/io.h>
+#include <util/delay.h>
 
 
-
-#define F_CPU 8000000UL			/* Define CPU Frequency e.g. here 8MHz */
-#include <avr/io.h>			/* Include AVR std. library file */
-#include <util/delay.h>			/* Include inbuilt defined Delay header file */
-
-#define LCD_Data_Dir DDRB		/* Define LCD data port direction */
-#define LCD_Command_Dir DDRC		/* Define LCD command port direction register */
-#define LCD_Data_Port PORTB		/* Define LCD data port */
-#define LCD_Command_Port PORTC		/* Define LCD data port */
-#define RS PC0				/* Define Register Select (data/command reg.)pin */
-#define RW PC1				/* Define Read/Write signal pin */
-#define EN PC2				/* Define Enable signal pin */
- 
+#define KEY_PRT 	PORTA
+#define KEY_DDR		DDRA
+#define KEY_PIN		PINA
 
 void LCD_Command(unsigned char cmnd)
 {
-	LCD_Data_Port= cmnd;
-	LCD_Command_Port &= ~(1<<RS);	/* RS=0 command reg. */
-	LCD_Command_Port &= ~(1<<RW);	/* RW=0 Write operation */
-	LCD_Command_Port |= (1<<EN);	/* Enable pulse */
-	_delay_us(1);
-	LCD_Command_Port &= ~(1<<EN);
-	_delay_ms(3);
-}
-
-void LCD_Char (unsigned char char_data)	/* LCD data write function */
-{
-	LCD_Data_Port= char_data;
-	LCD_Command_Port |= (1<<RS);	/* RS=1 Data reg. */
-	LCD_Command_Port &= ~(1<<RW);	/* RW=0 write operation */
-	LCD_Command_Port |= (1<<EN);	/* Enable Pulse */
-	_delay_us(1);
-	LCD_Command_Port &= ~(1<<EN);
-	_delay_ms(1);
-}
-
-void LCD_Init (void)			/* LCD Initialize function */
-{
-	LCD_Command_Dir = 0xFF;		/* Make LCD command port direction as o/p */
-	LCD_Data_Dir = 0xFF;		/* Make LCD data port direction as o/p */
-	_delay_ms(20);			/* LCD Power ON delay always >15ms */
+	LCD_DPRT = (LCD_DPRT & 0x0f)|(cmnd & 0xf0);		/* SEND COMMAND TO DATA PORT */
+	LCD_DPRT &= ~ (1<<LCD_RS);						/* RS = 0 FOR COMMAND */
+	LCD_DPRT |= (1<<LCD_EN);						/* EN = 1 FOR H TO L PULSE */
+	_delay_us(1);									/* WAIT FOR MAKE ENABLE WIDE */
+	LCD_DPRT &= ~(1<<LCD_EN);						/* EN = 0 FOR H TO L PULSE */
+	_delay_us(100);									/* WAIT FOR MAKE ENABLE WIDE */
 	
-	LCD_Command (0x38);		/* Initialization of 16X2 LCD in 8bit mode */
-	LCD_Command (0x0C);		/* Display ON Cursor OFF */
-	LCD_Command (0x06);		/* Auto Increment cursor */
-	LCD_Command (0x01);		/* Clear display */
-	LCD_Command (0x80);		/* Cursor at home position */
+	LCD_DPRT = (LCD_DPRT & 0x0f)|(cmnd << 4);		/* SEND COMMAND TO DATA PORT */
+	LCD_DPRT |= (1<<LCD_EN);						/* EN = 1 FOR H TO L PULSE */
+	_delay_us(1);									/* WAIT FOR MAKE ENABLE WIDE */
+	LCD_DPRT &= ~(1<<LCD_EN);						/* EN = 0 FOR H TO L PULSE */
+	_delay_us(2000);								/* WAIT FOR MAKE ENABLE WIDE */
 }
 
-void LCD_String (char *str)		/* Send string to LCD function */
+void LCD_Char(unsigned char data)
+{
+	LCD_DPRT = (LCD_DPRT & 0x0f)|(data & 0xf0);		/* SEND DATA TO DATA PORT */
+	LCD_DPRT |= (1<<LCD_RS);						/* MAKE RS = 1 FOR DATA */
+	LCD_DPRT |= (1<<LCD_EN);						/* EN=0 FOR H TO L PULSE */
+	_delay_us(1);									/* WAIT FOR MAKE ENABLE WIDE */
+	LCD_DPRT &= ~(1<<LCD_EN);						/* EN = 0 FOR H TO L PULSE */
+	_delay_us(100);									/* WAIT FOR MAKE ENABLE WIDE */
+	
+	LCD_DPRT = (LCD_DPRT & 0x0f)|(data << 4);		/*  */
+	LCD_DPRT |= (1<<LCD_EN);						/* EN=0 FOR H TO L PULSE*/
+	_delay_us(1);									/* WAIT FOR MAKE ENABLE WIDE*/
+	LCD_DPRT &= ~(1<<LCD_EN);						/* EN = 0 FOR H TO L PULSE*/
+	_delay_us(2000);								/* WAIT FOR MAKE ENABLE WIDE*/
+}
+
+void LCD_Init()
+{
+	LCD_DDDR = 0xFF;
+	_delay_ms(20);									/* WAIT FOR SOME TIME */
+	LCD_Command(0x02);								/* SEND $32 FOR INIT OT 0X02 */
+	LCD_Command(0x28);								/* INIT. LCD 2 LINE, 5 X 7 MATRIX */
+	LCD_Command(0x0C);								/* DISPLAY ON CURSOR ON */
+	LCD_Command(0x01);								/* LCD CLEAR */
+	LCD_Command(0x82);								/* SHIFT CURSOR TO WRITE */
+}
+
+void LCD_String_xy(unsigned char row, unsigned char pos, char *str)
+{
+	if (row == 1)
+	LCD_Command((pos & 0x0F)|0x80);				/* Command of first row and required position<16 */
+	else if (row == 2)
+	LCD_Command((pos & 0x0F)|0xC0);				/* Command of Second row and required position<16 */
+	
+	LCD_String(str);								/* Call LCD string function */
+}
+
+void LCD_String(char *str)
 {
 	int i;
-	for(i=0;str[i]!=0;i++)		/* Send each char of string till the NULL */
+	for(i=0;str[i]!=0;i++)							/* Send each char of string till the NULL */
 	{
-		LCD_Char (str[i]);
+		LCD_Char (str[i]);							/* Call LCD data write */
 	}
 }
 
-void LCD_String_xy (char row, char pos, char *str)/* Send string to LCD with xy position */
+void lcd_clear()
 {
-	if (row == 0 && pos<16)
-	LCD_Command((pos & 0x0F)|0x80);	/* Command of first row and required position<16 */
-	else if (row == 1 && pos<16)
-	LCD_Command((pos & 0x0F)|0xC0);	/* Command of first row and required position<16 */
-	LCD_String(str);		/* Call LCD string function */
+	LCD_Command(0x01);
+	_delay_ms(3);
+	LCD_Command(0x80);
 }
 
-void LCD_Clear()
+unsigned char keypad[4][4] = {	{'7','8','9','/'},
+								{'4','5','6','*'},
+								{'1','2','3','-'},
+								{' ','0','=','+'}};
+
+unsigned char colloc, rowloc;
+
+
+char keyfind()
 {
-	LCD_Command (0x01);		/* clear display */
-	LCD_Command (0x80);		/* cursor at home position */
+	while(1)
+	{
+		KEY_DDR = 0xF0;                /* set port direction as input-output */
+		KEY_PRT = 0xFF;
+
+		do
+		{
+			KEY_PRT &= 0x0F;           /* mask PORT for column read only */
+			asm("NOP");
+			colloc = (KEY_PIN & 0x0F); /* read status of column */
+		}while(colloc != 0x0F);
+		
+		do
+		{
+			do
+			{
+				_delay_ms(20);			   /* 20ms key debounce time */
+				colloc = (KEY_PIN & 0x0F); /* read status of column */
+			}while(colloc == 0x0F);        /* check for any key press */
+			
+			_delay_ms (40);				   /* 20 ms key debounce time */
+			colloc = (KEY_PIN & 0x0F);
+		}while(colloc == 0x0F);
+
+	/* now check for rows */
+		KEY_PRT = 0xEF;            /* check for pressed key in 1st row */
+		asm("NOP");
+		colloc = (KEY_PIN & 0x0F);
+		if(colloc != 0x0F)
+		{
+			rowloc = 0;
+			break;
+		}
+
+		KEY_PRT = 0xDF;			  /* check for pressed key in 2nd row */
+		asm("NOP");
+		colloc = (KEY_PIN & 0x0F);
+		if(colloc != 0x0F)
+		{
+			rowloc = 1;
+			break;
+		}
+		
+		KEY_PRT = 0xBF;			  /* check for pressed key in 3rd row */
+		asm("NOP");
+		colloc = (KEY_PIN & 0x0F);
+		if(colloc != 0x0F)
+		{
+			rowloc = 2;
+			break;
+		}
+
+		KEY_PRT = 0x7F;			  /* check for pressed key in 4th row */
+		asm("NOP");
+		colloc = (KEY_PIN & 0x0F);
+		if(colloc != 0x0F)
+		{
+			rowloc = 3;
+			break;
+		}
+	}
+
+	if(colloc == 0x0E)
+		return(keypad[rowloc][0]);
+	else if(colloc == 0x0D)
+		return(keypad[rowloc][1]);
+	else if(colloc == 0x0B)
+		return(keypad[rowloc][2]);
+	else
+		return(keypad[rowloc][3]);
 }
- 
-int main()
+
+int main(void)
 {
-
-	LCD_Init();			/* Initialize LCD */
-
-	LCD_String("Hello!!");	/* write string on 1st line of LCD*/
-	LCD_Command(0xC0);		/* Go to 2nd line*/
-	LCD_String("My Friends :)");	/* Write string on 2nd line*/
-
-	return 0;
+	LCD_Init();
+	LCD_String_xy(1,0,"Press a key");
+    while(1)
+	{
+		LCD_Command(0xc0);
+		LCD_Char(keyfind());             /* Display which key is pressed */
+	}
 }
