f38b8bf88b8fa11306db85a36c4b77834e4e6109
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-11-23 20:56:39 +0530

    Recreated new project for pip cutting machine

---
+++ PipeCuttingMachine/PipeCuttingMachine/main.c
@@ -0,0 +1,18 @@
+/*
+ * PipeCuttingMachine.c
+ *
+ * Created: 11/23/2021 7:01:38 PM
+ * Author : Dasith Rathnasinghe 
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
